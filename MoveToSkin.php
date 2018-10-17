90d7aba9f954c97c929ab21a1366bf9a5b8c3a33
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2018-10-17 21:12:49 +0300

    Fix to work with MediaWiki >= 1.29
    This mainly involves copying slightly more up-to-date code from
    extension:Skinny.
    
    Additional changes:
    - Convert to extension registration
    - Beautify code according to MW standards

--- MoveToSkin.php
+++ MoveToSkin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * MoveToSkin
- * A simple plugin which allows you to move content from a wiki article 
+ * A simple plugin which allows you to move content from a wiki article
  * to predefined areas in your skin.
  * Intended for MediaWiki Skin designers.
  * By Andru Vallance - andru@tinymighty.com
@@ -10,103 +10,67 @@
  *
  */
 
-$wgHooks['ParserFirstCallInit'][] = 'MoveToSkin::parserFirstCallInit';
-$wgHooks['LanguageGetMagic'][] = 'MoveToSkin::languageGetMagic';
-/* This was hooked to ParserAfterTidy, but that isn't called when the parse skep
-is skipped by retrieving from the parser cache */
-$wgHooks['OutputPageBeforeHTML'][] = 'MoveToSkin::moveContent';
+class MoveToSkin {
 
-$wgExtensionCredits['parserhook'][] = array(
-   'path' => __FILE__,
-   'name' => 'Move To Skin',
-   'description' => 'Move content from the article to the skin.',
-   'version' => '0.1.2', 
-   'author' => 'Andru Vallance',
-   'url' => 'https://www.mediawiki.org/wiki/Extension:MoveToSkin'
-);
+	public static $content = array();
 
-class MoveToSkin{
-  
-  public static $content = array();
-  
-  public static function parserFirstCallInit(&$parser){
-    $parser->setFunctionHook('movetoskin', 'MoveToSkin::parserFunction');
-    return true;
-  }
-  
-  public static function languageGetMagic(&$magicWords){
-    $magicWords['movetoskin'] = array(0,'movetoskin');
-    return true;
-  }
-  
-  public static function parserFunction($parser, $name='', $content=''){
-    //we have to wrap the inner content within <p> tags, because MW screws up otherwise by placing a <p> tag before and after with related closing and opening tags within
-    //php's DOM library doesn't like that and will swap the order of the first closing </p> and the closing </movetoskin> - stranding everything after that outside the <movetoskin> block. Lame.
-    //$content = $parser->recursiveTagParse($content);
-    $content = '<div class="movetoskin" data-target="'.$name.'">'.$content.'</div>';
-    return array( $content, 'noparse' => false, 'isHTML' => false );
-  }
-  
-  public static function moveContent(&$out, &$html){
-   if(empty($html))
-     return true;
-    //not sure why, but we have to UTF8 decode the html output... DOM Document is UTF8, so not sure why this is necessary, but without it UTF8 entities are garbled
-    $html = utf8_decode($html);
-    $doc = @DOMDocument::loadHTML( $html );
-    //$doc->encoding = 'utf8';
-    $xpath = new DomXPath($doc);
-    $nodes = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' movetoskin ')]");
-    //$movetoskins = $doc->getElementsByTagName('movetoskin');
-    
-    if($nodes->length > 0){
-      foreach($nodes as $move){
-        if($move->hasAttribute('data-target')){
-          $target = $move->getAttribute('data-target');
-          $inner_content = '';
-          
-          $move->removeAttribute('class');
-          $move->removeAttribute('data-target');
-          
-          $inner_content = trim($doc->saveHTML( $move ));
-          //chop off the starting <div> and ending </div> ... it's easier than trying to grab the contents via dom
-          $inner_content = substr($inner_content, 5, count($inner_content)-7);
-                    
-          if(isset( self::$content[ $target ] )){
-            array_push( self::$content[ $target ] , $inner_content );
-          }else{
-            self::$content[ $target ] = array(
-              $inner_content 
-            );
-          }
-          $parent = $move->parentNode;
-          $parent->removeChild($move);
-        }
-      }
-      
-      $htmldoc = $doc->saveHTML( $doc->getElementsByTagName('body')->item(0) );
-      if(preg_match('~<body>(.*?)<\/body>~si', $htmldoc, $match)){
-        $html = $match[1];
-      }
-      $html = $html;
-    }
+	public static function parserFirstCallInit( Parser &$parser ) {
+		$parser->setFunctionHook( 'movetoskin', 'MoveToSkin::parserFunction' );
 
-    return true;
-  }
-    
-  public static function hasContent($target){
-    if(isset(self::$content[$target]))
-      return true;
-    return false;
-  }
-  
-  public static function getContent($target=null){
-    if($target!==null){
-      if( self::hasContent($target) )
-        return self::$content[$target];
-      return array();
-    }else{
-      return self::$content;
-    }
-  }
+		return true;
+	}
+
+	public static function languageGetMagic( &$magicWords ) {
+		$magicWords[ 'movetoskin' ] = array( 0, 'movetoskin' );
+
+		return true;
+	}
+
+	public static function parserFunction( $parser, $name = '', $content = '' ) {
+		// we have to wrap the inner content within <p> tags, because MW screws up otherwise by
+		// placing a <p> tag before and after with related closing and opening tags within php's
+		// DOM library doesn't like that and will swap the order of the first closing </p> and the
+		// closing </movetoskin> - stranding everything after that outside the <movetoskin> block. Lame.
+		// $content = $parser->recursiveTagParse($content);
+		$content = '<ins data-type="movetoskin" data-name="' . $name . '">' . $content . '</ins>';
+
+		return [ $content, 'noparse' => false, 'isHTML' => false ];
+	}
+
+	public static function moveContent( &$out, &$html ) {
+		if ( empty( $html ) ) {
+			return $html;
+		}
+		if ( preg_match_all( '~<ins data-type="movetoskin" data-name="([\w:-]+)">([\S\s]*?)<\/ins>~m', $html, $matches, PREG_SET_ORDER ) ) {
+			foreach ( $matches as $match ) {
+				if ( !isset( self::$content[ $match[1] ] ) ) {
+					self::$content[ $match[1] ] = [];
+				}
+				array_push( self::$content[ $match[1] ], $match[2] );
+				$html = str_replace( $match[0], '', $html );
+			}
+		}
+		return true;
+	}
+
+	public static function hasContent( $target ) {
+		if ( isset( self::$content[ $target ] ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static function getContent( $target = null ) {
+		if ( $target !== null ) {
+			if ( self::hasContent( $target ) ) {
+				return self::$content[ $target ];
+			}
+
+			return array();
+		} else {
+			return self::$content;
+		}
+	}
 
 }
